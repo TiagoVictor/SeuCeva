4b1f196d3cbf76525649321060444ae9b27c50da
Tiago Victor <tiagovictordeoliveira@gmail.com>
2022-09-15 16:25:55 -0300

    Criação da repository de endereço + adicionando na Program para DI

---
+++ SeuCevApi/SeuCevApi/Data/Repository/EnderecoRepository.cs
@@ -0,0 +1,44 @@
+﻿using SeuCevApi.Data.Context;
+using SeuCevApi.Data.Repository.Interface;
+using SeuCevApi.Model;
+
+namespace SeuCevApi.Data.Repository
+{
+    public class EnderecoRepository : IEnderecoRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public EnderecoRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task Delete(Endereco endereco)
+        {
+            _applicationDbContext.Enderecos.Remove(endereco);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+
+        public async Task Edit(Endereco endereco)
+        {
+            _applicationDbContext.Enderecos.Update(endereco);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+
+        public IQueryable<Endereco> GetAll()
+        {
+            return _applicationDbContext.Enderecos.Where(x => x.Ativo);
+        }
+
+        public Endereco GetById(int id)
+        {
+            return _applicationDbContext.Enderecos.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public async Task Save(Endereco endereco)
+        {
+            await _applicationDbContext.Enderecos.AddAsync(endereco);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+    }
+}
