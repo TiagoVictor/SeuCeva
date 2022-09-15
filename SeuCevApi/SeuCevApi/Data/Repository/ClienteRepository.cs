5e413f0e6d993bc0953b763c2dbce74ff5acaaa7
Tiago Victor <tiagovictordeoliveira@gmail.com>
2022-09-15 15:47:13 -0300

    Criado repository de Cliente.

--- SeuCevApi/SeuCevApi/Data/Repository/ClienteRepository.cs
+++ SeuCevApi/SeuCevApi/Data/Repository/ClienteRepository.cs
@@ -1,8 +1,45 @@
-﻿using SeuCevApi.Data.Repository.Interface;
+﻿using Microsoft.EntityFrameworkCore;
+using SeuCevApi.Data.Context;
+using SeuCevApi.Data.Repository.Interface;
+using SeuCevApi.Model;
 
 namespace SeuCevApi.Data.Repository
 {
     public class ClienteRepository : IClienteRepository
     {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ClienteRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task Delete(Cliente cliente)
+        {
+            _applicationDbContext.Clientes.Remove(cliente);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+
+        public async Task Edit(Cliente cliente)
+        {
+            _applicationDbContext.Clientes.Update(cliente);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+
+        public IQueryable<Cliente> GetAll()
+        {
+            return _applicationDbContext.Clientes.Where(x => x.Ativo);
+        }
+
+        public Cliente GetById(int id)
+        {
+            return _applicationDbContext.Clientes.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public async Task Save(Cliente cliente)
+        {
+            await _applicationDbContext.Clientes.AddAsync(cliente);
+            await _applicationDbContext.SaveChangesAsync();
+        }
     }
 }
