06ef4b918a12e9b4754a8bd300e994aeaa8ed690
Tiago Victor <tiagovictordeoliveira@gmail.com>
2022-09-15 23:23:26 -0300

    Criando service de Tipo de Plano + adicionando na program para DI.

--- SeuCevApi/SeuCevApi/Service/Interface/ITipoPlanoService.cs
+++ SeuCevApi/SeuCevApi/Service/Interface/ITipoPlanoService.cs
@@ -1,6 +1,13 @@
-﻿namespace SeuCevApi.Service.Interface
+﻿using SeuCevApi.Model;
+
+namespace SeuCevApi.Service.Interface
 {
     public interface ITipoPlanoService
     {
+        public Task Save(TipoPlano tipoPlano);
+        public Task Delete(TipoPlano tipoPlano);
+        public Task Edit(TipoPlano tipoPlano);
+        public IQueryable<TipoPlano> GetAll();
+        public TipoPlano GetById(int id);
     }
 }
--- SeuCevApi/SeuCevApi/Service/TipoPlanoService.cs
+++ SeuCevApi/SeuCevApi/Service/TipoPlanoService.cs
@@ -1,8 +1,41 @@
-﻿using SeuCevApi.Service.Interface;
+﻿using SeuCevApi.Data.Repository.Interface;
+using SeuCevApi.Model;
+using SeuCevApi.Service.Interface;
 
 namespace SeuCevApi.Service
 {
     public class TipoPlanoService : ITipoPlanoService
     {
+        private readonly ITipoPlanoRepository _repository;
+
+        public TipoPlanoService(ITipoPlanoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Delete(TipoPlano tipoPlano)
+        {
+            await _repository.Delete(tipoPlano);
+        }
+
+        public async Task Edit(TipoPlano tipoPlano)
+        {
+            await _repository.Edit(tipoPlano);
+        }
+
+        public IQueryable<TipoPlano> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public TipoPlano GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public async Task Save(TipoPlano tipoPlano)
+        {
+            await _repository.Save(tipoPlano);
+        }
     }
 }
