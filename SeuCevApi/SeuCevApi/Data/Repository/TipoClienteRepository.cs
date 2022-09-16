2783f6dc740a8c7673d395608da3a10057aad00c
Tiago Victor <tiagovictordeoliveira@gmail.com>
2022-09-15 22:56:37 -0300

    Criação da repository do TipoCliente + adicionado na program para DI.

--- SeuCevApi/SeuCevApi/Data/Repository/TipoClienteRepository.cs
+++ SeuCevApi/SeuCevApi/Data/Repository/TipoClienteRepository.cs
@@ -1,8 +1,44 @@
-﻿using SeuCevApi.Data.Repository.Interface;
+﻿using SeuCevApi.Data.Context;
+using SeuCevApi.Data.Repository.Interface;
+using SeuCevApi.Model;
 
 namespace SeuCevApi.Data.Repository
 {
     public class TipoClienteRepository : ITipoClienteRepository
     {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public TipoClienteRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task Delete(TipoCliente tipoCliente)
+        {
+            _applicationDbContext.TiposClientes.Remove(tipoCliente);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+
+        public async Task Edit(TipoCliente tipoCliente)
+        {
+            _applicationDbContext.TiposClientes.Update(tipoCliente);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+
+        public IQueryable<TipoCliente> GetAll()
+        {
+            return _applicationDbContext.TiposClientes.Where(x => x.Ativo);
+        }
+
+        public TipoCliente GetById(int id)
+        {
+            return _applicationDbContext.TiposClientes.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public async Task Save(TipoCliente tipoCliente)
+        {
+            await _applicationDbContext.TiposClientes.AddAsync(tipoCliente);
+            await _applicationDbContext.SaveChangesAsync();
+        }
     }
 }
