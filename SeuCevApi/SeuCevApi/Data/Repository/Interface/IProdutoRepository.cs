ea413219f83cd0ffdcce62d05614102bbf1ed0de
Tiago Victor <tiagovictordeoliveira@gmail.com>
2022-09-15 22:29:11 -0300

    Criação da repository de Produto + adicionado na program para DI

--- SeuCevApi/SeuCevApi/Data/Repository/Interface/IProdutoRepository.cs
+++ SeuCevApi/SeuCevApi/Data/Repository/Interface/IProdutoRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace SeuCevApi.Data.Repository.Interface
+﻿using SeuCevApi.Model;
+
+namespace SeuCevApi.Data.Repository.Interface
 {
     public interface IProdutoRepository
     {
+        public Task Save(Produto produto);
+        public Task Delete(Produto produto);
+        public Task Edit(Produto produto);
+        public IQueryable<Produto> GetAll();
+        public Produto GetById(int id);
     }
 }
