784f90db60fc56735e841f706967a5c6662a6838
Tiago Victor de Oliveira <tiagovictordeoliveira@gmail.com>
2022-09-17 01:39:10 -0300

    Criada Service de documento + adicionado na program para DI

---
+++ SeuCevApi/SeuCevApi/Service/DocumentoService.cs
@@ -0,0 +1,41 @@
+﻿using SeuCevApi.Data.Repository.Interface;
+using SeuCevApi.Model;
+using SeuCevApi.Service.Interface;
+
+namespace SeuCevApi.Service
+{
+    public class DocumentoService : IDocumentoService
+    {
+        private readonly IDocumentoRepository _documentoRepository;
+
+        public DocumentoService(IDocumentoRepository documentoRepository)
+        {
+            _documentoRepository = documentoRepository;
+        }
+
+        public async Task Delete(Documento documento)
+        {
+            await _documentoRepository.Delete(documento);
+        }
+
+        public async Task Edit(Documento documento)
+        {
+            await _documentoRepository.Edit(documento);
+        }
+
+        public IQueryable<Documento> GetAll()
+        {
+            return _documentoRepository.GetAll();
+        }
+
+        public Documento GetById(int id)
+        {
+            return _documentoRepository.GetById(id);
+        }
+
+        public async Task Save(Documento documento)
+        {
+            await _documentoRepository.Save(documento);
+        }
+    }
+}
---
+++ SeuCevApi/SeuCevApi/Service/Interface/IDocumentoService.cs
@@ -0,0 +1,13 @@
+﻿using SeuCevApi.Model;
+
+namespace SeuCevApi.Service.Interface
+{
+    public interface IDocumentoService
+    {
+        public Task Save(Documento documento);
+        public Task Delete(Documento documento);
+        public Task Edit(Documento documento);
+        public IQueryable<Documento> GetAll();
+        public Documento GetById(int id);
+    }
+}
