0cf22004b6ebb30537fc844ccf545e7e0de5f542
Tiago Victor de Oliveira <tiagovictordeoliveira@gmail.com>
2022-09-17 02:04:24 -0300

    Criada service de Cliente + adicionada na program para DI

--- SeuCevApi/SeuCevApi/Service/ClienteService.cs
+++ SeuCevApi/SeuCevApi/Service/ClienteService.cs
@@ -1,8 +1,41 @@
-﻿using SeuCevApi.Service.Interface;
+﻿using SeuCevApi.Data.Repository.Interface;
+using SeuCevApi.Model;
+using SeuCevApi.Service.Interface;
 
 namespace SeuCevApi.Service
 {
     public class ClienteService : IClienteService
     {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteService(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        public async Task Delete(Cliente cliente)
+        {
+            await _clienteRepository.Delete(cliente);
+        }
+
+        public async Task Edit(Cliente cliente)
+        {
+            await _clienteRepository.Edit(cliente);
+        }
+
+        public IQueryable<Cliente> GetAll()
+        {
+            return _clienteRepository.GetAll();
+        }
+
+        public Cliente GetById(int id)
+        {
+            return _clienteRepository.GetById(id);
+        }
+
+        public async Task Save(Cliente cliente)
+        {
+            await _clienteRepository.Save(cliente);
+        }
     }
 }
--- SeuCevApi/SeuCevApi/Service/Interface/IClienteService.cs
+++ SeuCevApi/SeuCevApi/Service/Interface/IClienteService.cs
@@ -1,6 +1,13 @@
-﻿namespace SeuCevApi.Service.Interface
+﻿using SeuCevApi.Model;
+
+namespace SeuCevApi.Service.Interface
 {
     public interface IClienteService
     {
+        public Task Save(Cliente cliente);
+        public Task Delete(Cliente cliente);
+        public Task Edit(Cliente cliente);
+        public IQueryable<Cliente> GetAll();
+        public Cliente GetById(int id);
     }
 }
