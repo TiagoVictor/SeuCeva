7457720e6613ae0d62c23a06a4b2b60f6900a1e0
Tiago Victor <tiagovictordeoliveira@gmail.com>
2022-09-18 16:12:14 -0300

    Criado OfertaDto

---
+++ SeuCevApi/SeuCevApi/Dto/OfertaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SeuCevApi.Dto
+{
+    public class OfertaDto : BaseDto
+    {
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public int Alcance { get; set; }
+    }
+}
