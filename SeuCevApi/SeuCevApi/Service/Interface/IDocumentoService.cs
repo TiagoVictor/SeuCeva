9cb2471cfcfb02cabc35d7ad60f88c7bcfac9b55
Tiago Victor <tiagovictordeoliveira@gmail.com>
2022-10-03 22:21:09 -0300

    Criação da controller de Documento + adequação da service

--- SeuCevApi/SeuCevApi/Service/Interface/IDocumentoService.cs
+++ SeuCevApi/SeuCevApi/Service/Interface/IDocumentoService.cs
@@ -1,12 +1,13 @@
-﻿using SeuCevApi.Model;
+﻿using SeuCevApi.Dto;
+using SeuCevApi.Model;
 
 namespace SeuCevApi.Service.Interface
 {
     public interface IDocumentoService
     {
-        public Task Save(Documento documento);
-        public Task Delete(Documento documento);
-        public Task Edit(Documento documento);
+        public Task Save(DocumentoDto dto);
+        public Task Delete(DocumentoDto dto);
+        public Task Edit(DocumentoDto dto);
         public IQueryable<Documento> GetAll();
         public Documento GetById(int id);
     }
