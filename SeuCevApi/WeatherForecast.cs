155e2aee9d747715786e1cac915c6435c7d800fa
Tiago Victor de Oliveira <tiagovictordeoliveira@gmail.com>
2023-05-09 21:17:58 -0300

    Teste de github action

--- SeuCevApi/WeatherForecast.cs
+++ SeuCevApi/WeatherForecast.cs
@@ -9,5 +9,6 @@ namespace SeuCevApi
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public int Teste { get; set; }
     }
 }
